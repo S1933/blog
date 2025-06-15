af938abe351786958ee730dcd069fb79b75416f6
S1933 <jeanphilippenuel@gmail.com>
2025-06-15 15:10:55 +0200

    thrid commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,9 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://blog.s1933.ovh',
+  server: {
+    port: 1234,
+    host: true
+  },
   integrations: []
 });
